b6c6a66c5ed4d600831081c5089e6115a37bbdd3
Oscar Ahlén <github@ahlen.dev>
2025-12-01 13:15:50 +0100

    Tweak scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,6 @@ cd "$HOME/snowflake" || exit
 sed -i 's/user = "user";/user = "arch";/' "$HOME/snowflake/homes/user.nix"
 rm "$HOME/.bashrc"
 rm "$HOME/.bash_profile"
+rm "$HOME/.config/nix/nix.conf"
 
 nix-shell --arg sandbox false
